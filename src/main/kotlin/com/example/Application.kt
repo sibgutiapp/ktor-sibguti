2d4302d10a1ffc2d9e700c8b78200b159bc03051
Tim <ska1v3r@gmail.com>
2022-03-31 04:31:53 +0700

    procfile

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -7,7 +7,7 @@ import com.example.plugins.*
 const val API_VERSION = "/v1"
 
 fun main() {
-    embeddedServer(Netty, port = 5000) {
+    embeddedServer(Netty, port = 5000, host = "sibguti.herokuapp.com") {
         configureRouting()
         configureSerialization()
         configureTemplating()
